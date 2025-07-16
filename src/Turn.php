35852aa5e568a84a8ab9c1719a95164eacb497a2
Javier Canclini <jacancli@gmail.com>
2025-07-16 10:32:17 -0300

    refactor: simplify player retrieval methods and improve service handling

--- src/Turn.php
+++ src/Turn.php
@@ -12,14 +12,34 @@ class Turn
         private Player $service,
     ) {}
 
-    public function isService(Player $player): bool
+    public function getPlayer1(): Player
     {
-        return $this->service === $player;
+        return clone $this->player1;
     }
 
-    public function getOpponent(Player $player): Player
+    public function getPlayer2(): Player
     {
-        return $this->isService($player) ? $this->getRest() : $this->getService();
+        return clone $this->player2;
+    }
+
+    public function getService(): Player
+    {
+        return clone $this->service;
+    }
+
+    public function getRest(): Player
+    {
+        return $this->getPlayer1()->is($this->getService())
+            ? $this->getPlayer2()
+            : $this->getPlayer1();
+    }
+
+    /**
+     * @return array<int, Player> 
+     */
+    public function getPlayers(): array
+    {
+        return [$this->getPlayer1(), $this->getPlayer2()];
     }
 
     public function switch(): void
@@ -27,18 +47,14 @@ class Turn
         $this->service = $this->getRest();
     }
 
-    public function getService(): Player
-    {
-        return $this->service;
-    }
-
-    public function getRest(): Player
-    {
-        return $this->player1 === $this->service ? $this->player2 : $this->player1;
-    }
-
     public static function create(Player $player1, Player $player2): self
     {
         return new self($player1, $player2, $player1);
+    }
+
+    public static function createRandom(Player $player1, Player $player2): self
+    {
+        $service = rand(0, 1) === 0 ? $player1 : $player2;
+        return new self($player1, $player2, $service);
     }
 }
