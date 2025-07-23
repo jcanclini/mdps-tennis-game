18beed35481f4a037598b75e932908898dff0e07
Javier Canclini <jacancli@gmail.com>
2025-07-23 18:10:50 -0300

    refactor: streamline match creation and improve referee login logic in TennisController

--- src/TennisController.php
+++ src/TennisController.php
@@ -11,6 +11,8 @@ class TennisController
      */
     private array $players = [];
 
+    private Turn $turn;
+
     /**
      * @var array<int, Referee>
      */
@@ -22,22 +24,22 @@ class TennisController
      */
     private array $matches = [];
 
-    public function __construct(
-        private readonly Scoreboard $scoreboard
-    ) {}
+    public function __construct() {}
 
     public function createReferee(string $name, string $password): void
     {
         $this->referees[] = new Referee(count($this->referees) + 1, $name, $password);
     }
 
-    public function login(string $name, string $password): void
+    public function login(string $name, string $password): bool
     {
         foreach ($this->referees as $key => $referee) {
             if ($referee->areCredentialsValid($name, $password)) {
                 $this->loggedReferee = $key;
+                return true;
             }
         }
+        return false;
     }
 
     public function logout(): void
@@ -57,21 +59,55 @@ class TennisController
         $this->players[$player->getId()] = $player;
     }
 
+    /**
+     * Create a match with the given players and sets to play.
+     * 
+     * @param array<int, Player> $players 
+     * @param int $setsToPlay 
+     * @return void 
+     */
     public function createMatch(
-        Player $player1,
-        Player $player2,
+        array $players,
         int $setsToPlay
     ): void {
         assert($this->loggedReferee !== null, 'You must be logged in to create a match.');
 
+        $this->turn = Turn::createRandom($players);
+
         $this->matches[] = new TennisMatch(
             count($this->matches) + 1,
-            $player1,
-            $player2,
+            $this->turn,
             $setsToPlay
         );
+    }
 
-        $this->scoreboard->setMatch($this->currentMatch());
+    public function addPointToService(): void
+    {
+        assert($this->getCurrentMatch() !== null, 'No match created yet.');
+        $this->getCurrentMatch()->addPointTo($this->turn->getService());
+    }
+
+    public function addPointToRest(): void
+    {
+        assert($this->getCurrentMatch() !== null, 'No match created yet.');
+        $this->getCurrentMatch()->addPointTo($this->turn->getRest());
+    }
+
+    public function lackService(): void
+    {
+        assert($this->getCurrentMatch() !== null, 'No match created yet.');
+        $this->getCurrentMatch()->lackService();
+    }
+
+    public function getScoreboard(): Scoreboard
+    {
+        assert($this->getCurrentMatch() !== null, 'No match created yet.');
+
+        return $this->getCurrentMatch()
+            ->getScoreboard()
+            ->setTurn($this->turn)
+            ->setReferee($this->referees[$this->loggedReferee])
+            ->setCurrentMatch($this->getCurrentMatch());
     }
 
     /**
@@ -82,9 +118,11 @@ class TennisController
         return $this->players;
     }
 
-    public function getReferee(int $id): ?Referee
+    public function currentMatchIsFinished(): bool
     {
-        return $this->referee[$id] ?? null;
+        assert($this->getCurrentMatch() !== null, 'No match created yet.');
+
+        return $this->getCurrentMatch()->isFinished();
     }
 
     public function getPlayer(int $id): ?Player
@@ -92,33 +130,11 @@ class TennisController
         return $this->players[$id] ?? null;
     }
 
-    public function getScore(): array
-    {
-        return $this->scoreboard->getScore();
-    }
-
-    public function getBoard(): Scoreboard
-    {
-        return $this->scoreboard;
-    }
-
-    public function currentMatch(): ?TennisMatch
-    {
-        return end($this->matches) ?: null;
-    }
-
-    public function getMatchId(): int
-    {
-        assert($this->currentMatch() !== null, 'No match created yet.');
-
-        return $this->currentMatch()->getId();
-    }
-
     public function getMatchDate(): string
     {
-        assert($this->currentMatch() !== null, 'No match created yet.');
+        assert($this->getCurrentMatch() !== null, 'No match created yet.');
 
-        return $this->currentMatch()->getDate()->format('Y-m-d H:i:s');
+        return $this->getCurrentMatch()->getDate()->format('Y-m-d H:i:s');
     }
 
     public function getRefereeName(): string
@@ -126,5 +142,10 @@ class TennisController
         assert($this->loggedReferee !== null, 'No referee logged in.');
 
         return $this->referees[$this->loggedReferee]->getName();
+    }
+
+    private function getCurrentMatch(): ?TennisMatch
+    {
+        return $this->matches[count($this->matches) - 1] ?? null;
     }
 }
