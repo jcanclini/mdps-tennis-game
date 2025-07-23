58b46a775cf4349ffe5d644eb84b5b088b78fd95
Javier Canclini <jacancli@gmail.com>
2025-07-23 18:11:13 -0300

    refactor: simplify TennisMatch constructor and enhance scoreboard integration

--- src/TennisMatch.php
+++ src/TennisMatch.php
@@ -15,19 +15,15 @@ class TennisMatch
      */
     private array $sets = [];
 
-    private Turn $turn;
-
     private \DateTimeImmutable $date;
 
     public function __construct(
         private readonly int $id,
-        Player $player1,
-        Player $player2,
+        private Turn $turn,
         private readonly int $setsToPlay
     ) {
         assert(in_array($setsToPlay, self::ALLOWED_SETS), 'Max sets must be either 3 or 5.');
-        $this->turn = Turn::createRandom([$player1, $player2]);
-        $this->sets[] = $this->createSet();
+        $this->createSet();
         $this->date = new \DateTimeImmutable();
     }
 
@@ -45,9 +41,19 @@ class TennisMatch
     {
         assert($this->isFinished() === false, 'Match is already finished.');
         $this->currentSet()->addPointTo($player);
-        if ($this->currentSet()->isFinished()) {
-            $this->sets[] = $this->createSet();
+        if (!$this->isFinished() && $this->currentSet()->isFinished()) {
+            $this->createSet();
         }
+    }
+
+    public function getScoreboard(): Scoreboard
+    {
+        return $this->currentSet()
+            ->getScoreboard()
+            ->setSets($this->sets)
+            ->setMatchFinished($this->isFinished())
+            ->setPendingSets($this->getPendingSets())
+            ->setMatchBall($this->isMatchBall());
     }
 
     /**
@@ -63,44 +69,14 @@ class TennisMatch
         return $this->setsToPlay - count($this->sets);
     }
 
-    public function getPlayerPoints(Player $player): int
-    {
-        return $this->currentSet()->getCurrentGame()->getPoints($player);
-    }
-
-    public function hasLackService(): bool
-    {
-        return $this->currentSet()->getCurrentGame()->isLackService();
-    }
-
     public function lackService(): void
     {
-        $this->currentSet()->getCurrentGame()->lackService();
-    }
-
-    public function getCurrentGameService(): Player
-    {
-        return $this->turn->getService();
-    }
-
-    public function isGameBall(): bool
-    {
-        return $this->currentSet()->getCurrentGame()->isGameBall();
-    }
-
-    public function isSetBall(): bool
-    {
-        return $this->currentSet()->isSetBall();
+        $this->currentSet()->lackService();
     }
 
     public function isMatchBall(): bool
     {
-        return $this->getPendingSets() === 1 && $this->currentSet()->isSetBall();
-    }
-
-    public function isTieBreak(): bool
-    {
-        return $this->currentSet()->isTieBreak();
+        return $this->getPendingSets() === 1 && $this->currentSet()->getScoreboard()->isSetBall();
     }
 
     public function isFinished(): bool
@@ -119,31 +95,21 @@ class TennisMatch
         return $this->getMinSetsToWin() === count(array_filter($this->sets, fn(Set $set) => $set->isWinner($player)));
     }
 
-    /**
-     * @return array<int, Player> 
-     */
-    public function getPlayers(): array
-    {
-        return $this->turn->getPlayers();
-    }
-
     public function getMinSetsToWin(): int
     {
         return intdiv($this->setsToPlay, 2) + 1;
     }
 
-    private function createSet(): Set
+    private function createSet(): void
     {
-        $this->turn->switch();
-
-        return new Set(
-            count($this->sets) + 1,
+        $this->sets[] = new Set(
+            rand(1, 1000),
             $this->turn
         );
     }
 
     private function currentSet(): Set
     {
-        return end($this->sets) ?: $this->createSet();
+        return $this->sets[count($this->sets) - 1];
     }
 }
